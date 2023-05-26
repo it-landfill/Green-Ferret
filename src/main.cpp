f7573b1b1c9747e23036f63b9ac36528c211b564
Alessandro Benetton <aleben98@gmail.com>
2023-05-26 13:51:30 +0200

    [develop] Send actual sensor data

--- src/main.cpp
+++ src/main.cpp
@@ -10,37 +10,52 @@
 #include "sensors/bmp280.hpp"
 #include "sensors/ens160.hpp"
 
+// Switch between WROOM32 pin and HELTEC pin
+#ifdef WROOM32
+#define SDA_PIN 21
+#define SCL_PIN 22
+#else
 #define SDA_PIN 41
 #define SCL_PIN 42
+#endif
 
 void setup(){
 	Serial.begin(115200);
 	logInfo("MAIN", "Starting Setup");
 	wifiSetup(true);
-	// setupTime();
-	// syncTimeServer();
-	
+
 	// Connect to the remote broker and subscribe to the topic
 	mqttSetup();
 	mqttConnect();
 
 	// Sensor setup
-	//Wire.begin(SDA_PIN, SCL_PIN);
-	//aht20Setup();
-	//ens160Setup(aht20GetTemperature(), aht20GetHumidity());
-	//bmp280Setup();
+	Wire.begin(SDA_PIN, SCL_PIN);
+	aht20Setup();
+	ens160Setup(aht20GetTemperature(), aht20GetHumidity());
+	bmp280Setup();
 
 	logInfo("MAIN", "Setup Complete");
 }
 
+int counter = 0;
+
 void loop(){
-	delay(5000);
-	float temperature = random(20, 25);
-	float humidity = random(50, 60);
-	float pressure = random(1000, 1010);
-	float x = random(11, 12);
-	float y = random(44, 45);
-	char *jsonMsg = serializeSensorData(&temperature, &humidity, &pressure, &x, &y);
+	float temperature = bmp280ReadTemperature();
+	float humidity = aht20GetHumidity();
+	float lat = 45.666668;
+	float lon = 12.250000;
+	int aqi = ens160GetAQI();
+	int tvoc = ens160GetTVOC();
+	int eco2 = ens160GetECO2();
+	char* jsonMsg = NULL;
+
+	if (counter++ == 6) { // Every minute
+		float pressure = bmp280ReadPressure();
+		jsonMsg = serializeSensorData(&temperature, &humidity, &pressure, &lat, &lon, &aqi, &tvoc, &eco2);
+	} else {
+		jsonMsg = serializeSensorData(&temperature, &humidity, NULL, &lat, &lon, &aqi, &tvoc, &eco2);
+	}
 	logInfof("MAIN", "Json to be pubblish: %s", jsonMsg);
 	mqttPublishSensorData(jsonMsg);
+	delay(10000); // Sleep for 10 seconds
 }
